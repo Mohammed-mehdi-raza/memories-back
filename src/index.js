e72e310f33ecd4d4a9ad173e971ddd0e534de0a4
Mohammed-mehdi-raza <mehdirazajafri2040@gmail.com>
2022-04-20 19:15:50 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const app=express();
 app.use(cors());
 app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({extended:false,limit:'50mb'}));
-const PORT=process.env.port||5000;
+const PORT=process.env.PORT||5000;
 
 app.use('/posts',PostRoutes);
 
